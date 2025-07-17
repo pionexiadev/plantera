6357b10444275c499bea8d4f373c4d4a460e3be2
pionexiadev <ibrahimalo407@gmail.com>
2025-07-17 09:04:57 +0100

    Auth added

--- src/main/java/com/exemple/agriculture/security/SecurityConfig.java
+++ src/main/java/com/exemple/agriculture/security/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
+                		"/auth/**", 
                     "/api/auth/**",
                     "/api/betail/**",
                     "/api/parcelles/**",
